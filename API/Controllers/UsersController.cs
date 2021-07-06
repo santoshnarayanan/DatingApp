7230f90a12e180b21d657bcc7484267d29f5973b
santosh33@gmail.com <Password1234567890!@#>
2021-07-06 23:09:42 +0530

    DatingApp  API code checked IN (Chapter1-4)

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entites;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class UsersController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
