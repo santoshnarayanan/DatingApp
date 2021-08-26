c43f50db06ff9687fbe2d074f9a0391c03854be7
Santosh Narayanan <32931478+santoshnarayanan@users.noreply.github.com>
2021-08-27 03:08:25 +0530

    Create Angular Form template (FormModule)

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model:any ={}
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login():void{
+    console.log(this.model);
   }
 
 }
