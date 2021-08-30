2464abe2fc79ff603e57292bf7a9b13d946dc804
santosh33 <santosh33@gmail.com>
2021-08-30 12:37:55 +0530

    Adding routing info in the code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { AccountService } from '../_services/account.service';
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
 import {User} from "../_models/user";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
   model:any ={};
   currentUser$ : Observable<User> | undefined;
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$;
@@ -20,6 +21,7 @@ export class NavComponent implements OnInit {
 
   login():void{
     this.accountService.login(this.model).subscribe(response=> {
+      this.router.navigateByUrl('/members');
       console.log(response);
     }, error=> {
       console.log(error);
@@ -28,5 +30,6 @@ export class NavComponent implements OnInit {
 
   logout():void{
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
