b30e20fb477f2a71b8296c58634cc6ab34a8bea0
santosh33 <santosh33@gmail.com>
2021-08-30 13:14:11 +0530

    Adding toast for notification

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
 import {User} from "../_models/user";
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +14,8 @@ export class NavComponent implements OnInit {
   model:any ={};
   currentUser$ : Observable<User> | undefined;
 
-  constructor(private accountService: AccountService, private router: Router) { }
+  constructor(private accountService: AccountService, private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$;
@@ -25,6 +27,7 @@ export class NavComponent implements OnInit {
       console.log(response);
     }, error=> {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
