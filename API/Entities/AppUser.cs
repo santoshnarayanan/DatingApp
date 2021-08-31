78d76b02c97e679f42978c86a5721e920703c87b
santosh33 <santosh33@gmail.com>
2021-09-01 01:30:08 +0530

    Adding a DateTime extension to calculate age

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using API.Extensions;
+using System;
 using System.Collections.Generic;
 
 namespace API.Entities
@@ -25,6 +26,13 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
+
 
     }
 }
