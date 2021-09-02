0962cc36e19e617f86a42528e635d676bcc990db
santosh33 <santosh33@gmail.com>
2021-09-02 23:28:44 +0530

    Using AutoMapper

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -9,12 +9,10 @@ namespace API.DTOs
     public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
         public int Age { get; set; }
-
         public string KnownAs { get; set; }
-
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string Gender { get; set; }
